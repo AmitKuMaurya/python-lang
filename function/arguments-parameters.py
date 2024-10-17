dcd09ab217ea97f2cd51e80e7a1c5a37db22e250
AmitKuMaurya <amit.maurya@veup.in>
2024-10-18 01:43:04 +0530

    Basics of python in function and sets is done

--- function/arguments-parameters.py
+++ function/arguments-parameters.py
@@ -4,3 +4,41 @@ def greet(name,message):
     print(f"Hey, {name}!, {message} ");
 
 greet("amit","you will win a day !");
+
+
+
+# normal way to pass and receive arguments.
+# we can pass multiple arguments also there.
+
+def test(params) :
+    print(params)
+
+test("hello");
+
+
+# default arguments.
+# first param ---->> Can never be default argument
+
+def test(day,msg,end="Have, a good day !") :
+    print(day,msg,end);
+
+test("Friday",", ist aufregend!");
+
+
+
+# arbitary aguments --> *args
+
+# this is basically to accept/pass as arguments any type of data-structure instead of [key:value]
+
+def hello(*args):
+    print(args);
+
+hello("gutan tag","guten aufregend","tchuss","bis spater!");
+
+# keyword arguments --> **kwargs
+# this accepts args/params in [key:value] pair.
+
+def mutliArgs(**kwargs):
+    print(kwargs);
+
+mutliArgs(name="Alice", age=30, city="New York");
